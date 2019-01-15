1ec04e6c044e7eae810f2ec10e817ca3f88a59c6
Daniel Frey <dann.frey@me.com>
2019-01-14 16:39:09 -0800

    fixed views

--- project_data/views.py
+++ project_data/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-from django.views.decorators.cache import cache_page
 from django.conf import settings
 from django.core.paginator import Paginator
 from .models import Kickstarter
 
 
-CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
-
-
-@cache_page(CACHE_TTL)
 def kickstarter_list_view(request):
     """Using paginator we will display 20 results per page.
     """
@@ -19,7 +13,6 @@ def kickstarter_list_view(request):
 
     page = request.GET.get('page')
     kickstarters = paginator.get_page(page)
-
 
     context = {
         'kickstarters': kickstarters
